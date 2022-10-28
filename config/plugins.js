9116ecde51ed396d582934a2acc24a5b4be95c1d
Manohar <dev.manoharkumar@gmail.com>
2022-10-29 02:32:56 +0530

    Derp

--- config/plugins.js
+++ config/plugins.js
@@ -8,7 +8,7 @@ module.exports = ({env}) => ({
   },
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: 'strapi-provider-upload-aws-s3-advanced',
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
